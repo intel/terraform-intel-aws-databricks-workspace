567d1f45377c8f1a6ef14051b06b1f79254a16c2
shreejam21 <shreejan.mistry@intel.com>
2023-03-07 09:51:26 -0800

    variables.tf updated

--- examples/databricks-workspace/variables.tf
+++ examples/databricks-workspace/variables.tf
@@ -7,19 +7,15 @@ variable "region" {
 variable "dbx_account_password" {
   type = string
   description = "Account Login Password for the Databricks Account"
-  default = "Blackdown2112!"
 }
 
 variable "dbx_account_username" {
   type        = string
   description = "Account Login Username for the Databricks Account"
-  default = "shreejan.mistry@intel.com"
 }
 
-#f36297ba-ff38-4b4b-b5be-0a14d4b6d095"
 variable "dbx_account_id" {
   type = string
   description = "Account Login Username for the Databricks Account"
-  default = "f36297ba-ff38-4b4b-b5be-0a14d4b6d095"
 }
 
